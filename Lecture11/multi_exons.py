c8027765c912b04cd59266f6f60507b2042af4fd
B155285-2019 <s1990277@ed.ac.uk>
2019-10-27 12:19:08 +0000

    saved as fasta ex2

--- Lecture11/multi_exons.py
+++ Lecture11/multi_exons.py
@@ -2,12 +2,13 @@
 dna_seq = open("genomic_dna2.txt").read().rstrip('\n')
 exons_info = open("exons.txt").read().rstrip('\n')
 
-out_file = open("concat_exons.txt", "w")
+out_file = open("concat_exons_ex2.fasta", "w")
 exons_lines = exons_info.split('\n')
 
 concat_exons = ''
 for lines in exons_lines:
 	st_end = lines.split(',')
 	concat_exons += dna_seq[(int(st_end[0])-1):int(st_end[1])]
-	print(dna_seq[(int(st_end[0])-1):int(st_end[1])])
+	#print(dna_seq[(int(st_end[0])-1):int(st_end[1])])
+out_file.write(">Lecture10_exercise2_exons"+"\n")
 out_file.write(concat_exons)
